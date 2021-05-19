7289c4fae3d04c9e162c85ed49dac84ecc8c7532
ArnaudPB <arnaud.besson@gmail.com>
2021-05-19 16:33:48 +0200

    [rm] removed non working files

--- js/utility.js
+++
@@ -1,7 +0,0 @@
-// Variables
-var panier = JSON.parse(localStorage.getItem('panier')) || [];
-
-const itemCounter = document.querySelector(".cart-count");
-
-// Item Cart-Counter
-itemCounter.textContent = "( " + panier.length + " )";
